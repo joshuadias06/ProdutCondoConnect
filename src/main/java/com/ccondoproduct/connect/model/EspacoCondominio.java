8bc4d0cb2aee9361dac2361afd4fc4dd9a141900
Joshua Dias <joshuadiasbf@gmail.com>
2025-03-01 11:35:25 -0300

    Create Enum EspacoCondominio

---
+++ src/main/java/com/ccondoproduct/connect/model/EspacoCondominio.java
@@ -0,0 +1,10 @@
+package com.ccondoproduct.connect.model;
+
+public enum EspacoCondominio {
+    SALAO_DE_FESTAS,
+    PISCINA,
+    CHURRASQUEIRA,
+    QUADRA,
+    ACADEMIA,
+    ESPACO_GOURMET
+}
