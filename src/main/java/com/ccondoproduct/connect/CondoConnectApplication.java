bd998752b0a2d80220af76d7dabb0a8ea89b8d30
Joshua Dias <joshuadiasbf@gmail.com>
2025-03-01 11:01:38 -0300

    Start Project

---
+++ src/main/java/com/ccondoproduct/connect/CondoConnectApplication.java
@@ -0,0 +1,13 @@
+package com.ccondoproduct.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondoConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondoConnectApplication.class, args);
+	}
+
+}
