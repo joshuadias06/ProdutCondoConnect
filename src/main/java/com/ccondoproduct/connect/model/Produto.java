c58f0b75a58f621595bca198908c61bd46641432
Joshua Dias <joshuadiasbf@gmail.com>
2025-03-01 11:09:11 -0300

    Create Entity Produto

---
+++ src/main/java/com/ccondoproduct/connect/model/Produto.java
@@ -0,0 +1,31 @@
+package com.ccondoproduct.connect.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private int quantidade;
+}
