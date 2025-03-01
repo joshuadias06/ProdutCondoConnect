466b9bb2772c894af54f6aa0375d831f1e431af8
Joshua Dias <joshuadiasbf@gmail.com>
2025-03-01 14:11:05 -0300

    Created LocalizacaoRepository

---
+++ src/main/java/com/ccondoproduct/connect/repository/LocalizacaoRepository.java
@@ -0,0 +1,7 @@
+package com.ccondoproduct.connect.repository;
+
+import com.ccondoproduct.connect.model.Localizacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocalizacaoRepository extends JpaRepository<Localizacao, Long> {
+}
