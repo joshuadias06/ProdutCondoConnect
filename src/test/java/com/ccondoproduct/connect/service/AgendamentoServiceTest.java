148d63951110af675992a53e357d124ca714eccd
anafsenac <ana.lcfranchi@senacsp.edu.br>
2025-04-14 21:34:36 -0300

    import mokito

--- src/test/java/com/ccondoproduct/connect/service/AgendamentoServiceTest.java
+++ src/test/java/com/ccondoproduct/connect/service/AgendamentoServiceTest.java
@@ -1,4 +1,8 @@
 package com.ccondoproduct.connect.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class AgendamentoServiceTest {
 }
